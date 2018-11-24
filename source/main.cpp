7a397a479b8f0735dd59fd9e82dec3eae653c5ac
Matthew Scholefield <matthew331199@gmail.com>
2018-11-24 00:17:06 -0600

    Use CLI11 to parse arguments

--- source/main.cpp
+++ source/main.cpp
@@ -4,12 +4,26 @@
 #include "SdlWindow.hpp"
 #include "SoundSystem.hpp"
 #include "Instrument.hpp"
+#include "CLI11.hpp"
 
 
 int main(int argc, char **argv) {
+  CLI::App app{"Dynamic Music Generation App"};
+  app.ignore_case();
+
+  std::string instrument_file;
+  float gain = 0.f;
+  app.add_option("soundfont_file", instrument_file, "Soundfont file (.sf2) to play song with")->required();
+  app.add_option("-g,--gain", gain, "Volume gain of instrument (negative or positive decimal)");
+
+  CLI11_PARSE(app, argc, argv);
+
+  std::cout << "Loading instrument..." << std::endl;
+  Instrument instrument(instrument_file, gain);
+  std::cout << "Starting app..." << std::endl;
+
   SdlWindow window;
   Renderer renderer(window.GetRaw());
-  Instrument instrument(argv[1]);
   SoundSystem system([&](float *f, size_t n) {
     instrument.render(f, n);
     return 0;
