fa2e012beb4f75799b160d760f3271f37ab920a0
Matthew Scholefield <matthew331199@gmail.com>
2018-12-07 18:08:34 -0600

    Fix interface bugs

--- source/Interface.cpp
+++ source/Interface.cpp
@@ -2,14 +2,15 @@
 
 Interface::Interface(SongGenerator &generator, SdlWindow &window) : generator(generator) {
     window.onTouchInput([&](float x, float y) { onTouchInput(x, y); });
+    generator.setDelta(bx);
 }
 
 void Interface::render(Renderer &renderer) {
-    renderer.rect(bx, by, 0.1f, 0.1f, {200, 200, 180});
+    renderer.rect(bx - 0.05f, by - 0.05f, 0.1f, 0.1f, {200, 200, 180});
 }
 
 void Interface::onTouchInput(float x, float y) {
-    bx = x;
-    by = y;
-    generator.setDelta(x);
+    bx = std::max(0.f, std::min(1.f, x));
+    by = std::max(0.f, std::min(1.f, y));
+    generator.setDelta(0.1f + bx);
 }
