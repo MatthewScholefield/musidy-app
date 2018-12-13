379c05a4d2b5f9ef09cf1f68a05a33d4904633e5
Matthew Scholefield <matthew331199@gmail.com>
2018-12-12 21:42:50 -0600

    Fix documentation

--- source/utils.hpp
+++ source/utils.hpp
@@ -11,6 +11,10 @@
  * @return Index of randomly chosen element
  */
 int selectSample(const std::vector<float> &probs);
+
+/**
+ * @return A random float between 0.0 and 1.0
+ */
 float randFloat();
 
 template<typename T>
@@ -23,6 +27,13 @@ T min(T a, T b) {
     return a < b ? a : b;
 }
 
+/**
+ * Return the minimum value of a sequence
+ * @param a First value
+ * @param b Second value
+ * @param args Other values
+ * @return Minimum of all values
+ */
 template<typename T, typename...Args>
 T min(T a, T b, Args...args) {
     return min(min(a, b), args...);
@@ -38,6 +49,14 @@ T minTo(T t, T a, T b) {
     return abs(t - a) < abs(t - b) ? a : b;
 }
 
+/**
+ * Return the value with the minimum distance to a target
+ * @param t Target value
+ * @param a First value
+ * @param b Second value
+ * @param args Other values
+ * @return Value closest to the target
+ */
 template<typename T, typename...Args>
 T minTo(T t, T a, T b, Args...args) {
     return minTo(t, minTo(t, a, b), args...);
